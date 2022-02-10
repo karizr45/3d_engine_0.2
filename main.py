2af3d916a1fe189460d429a598b7381891f50f23
karizr45 <karizr45@gmail.com>
2022-02-10 17:34:36 +0500

    new update!

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from setting import *
 
 display = pygame.display.set_mode(width, height)
 clock = pygame.time.clock()
+
+while True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    display.fill((0, 0, 0))
+
+    clock.tick()
+    pygame.display.flip()
