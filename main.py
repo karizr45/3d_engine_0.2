690dce315bf8c4166063792114e84b76c4dbdb64
karizr45 <karizr45@gmail.com>
2022-02-10 17:28:49 +0500

    idk

---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame
+from setting import *
+
+display = pygame.display.set_mode(width, height)
+clock = pygame.time.clock()
---
+++ setting.py
@@ -0,0 +1,5 @@
+#screen
+width = 1200
+height = 800
+half_height = height / 2
+half_width = width / 2
